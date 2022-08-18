264d23dff529b5331a3b21eabd5518f4b320d444
Junaid <junaid@yarikul.in>
2022-08-18 21:47:14 +0530

    feat: Adds support to get all the menu items

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -94,6 +94,6 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
-        throw new \Exception('implement in coding task 3');
+        $data = MenuItem::all();
     }
 }
